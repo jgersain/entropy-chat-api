0fd226337c56d83220ef7b14c96b9970f01de4a0
jgersain <jhersein@gmail.com>
2019-12-14 03:57:54 -0600

    Create endpoints for login and register a new user

---
+++ api/controllers/routes_controller.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"github.com/jgersain/entropy-chat-api/api/middlewares"
+)
+
+func (server *Server) initializeRoutes() {
+
+	server.Router.HandleFunc("/login", middlewares.SetMiddlewareJSON(server.Login)).Methods("POST")
+	server.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON(server.CreateUser)).Methods("POST")
+	server.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(server.UpdateUser))).
+		Methods("PUT")
+}
